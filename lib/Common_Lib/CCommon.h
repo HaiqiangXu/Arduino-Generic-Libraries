47efbe662491884b6e652aad64a140b0c30f9699
Haiqiang Xu <haiqiang@hotmail.es>
2024-07-09 19:45:53 +0200

    Closes #9 leaving classes that are compatible with any HW that supports Arduino Framework

--- lib/Common_Lib/CCommon.h
+++ lib/Common_Lib/CCommon.h
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <LowPower.h>
 
 typedef struct
 {
@@ -25,88 +24,9 @@ public:
         return stringValue;
     };
 
-    static void PowerDownInt0()
-    {
-        // NOTE: Seems that FALLING/RISING/CHANGE signals doesn't work to wake-up arduino from PowerDown, perhaps because pin is configured as INPUT_PULLUP.
-        // Allow wake up interrupt when input pin detects LOW signal.
-        // Wake-up pin attached to Interrupt 0 which corresponds I/O pin 2 on Uno/Nano.
-        attachInterrupt(0, WakeUpInt0, LOW);
-
-        // Enter power down state with ADC and BOD module disabled. Wake up when interrupt detected
-        LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
-        ///delay(75); // allows the arduino to fully wake up.
-    };
-
-    static void PowerDownInt1()
-    {
-        // Same as PowerDownInt0() but wake-up pin attached to Interrupt 1 which corresponds I/O pin 3 on Uno/Nano.
-        attachInterrupt(1, WakeUpInt1, LOW);
-        LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
-    };
-
-    static void Sleep(period_t period)
-    {
-        //NOTE: Nano transmits garbage characters to PC through HardwareSerial if USART0 is OFF
-        //NOTE2: every time sleeps and wake-ups, the millis() value is RESET, so take care when using Sleep() if millis() is used in program
-        LowPower.idle(period, ADC_ON, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_ON, TWI_OFF);
-    };
-
-    static void Sleep(int iPeriod)
-    {
-        period_t period;
-        switch (iPeriod)
-        {
-            case 15:
-                period = SLEEP_15MS;
-                break;
-            case 30:
-                period = SLEEP_30MS;
-                break;
-            case 60:
-                period = SLEEP_60MS;
-                break;
-            case 120:
-                period = SLEEP_120MS;
-                break;
-            case 250:
-                period = SLEEP_250MS;
-                break;
-            case 500:
-                period = SLEEP_500MS;
-                break;
-            case 1000:
-                period = SLEEP_1S;
-                break;
-            case 2000:
-                period = SLEEP_2S;
-                break;
-            case 4000:
-                period = SLEEP_4S;
-                break;
-            case 8000:
-                period = SLEEP_8S;
-                break;
-            default:
-                period = SLEEP_250MS;   //default period
-                break;
-        }
-        LowPower.idle(period, ADC_ON, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_ON, TWI_OFF);
-    };
 private:
     // Disable creating instances of this object
     CCommon() { };
-
-    static void WakeUpInt0()
-    {
-        // Just a handler for the pin interrupt.
-        // Disable external interrupt on wake up pin.
-        detachInterrupt(0);
-    };
-
-   	static void WakeUpInt1()
-    {
-        detachInterrupt(1);
-    };
 };
 
 //instance for use in external libraries to access directly to static methods
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,24 @@
 #include <MD_MAX72xx.h>
-#include <TrueRandom.h>
 #include <cppQueue.h>
 #include <LinkedList.h>
 
 #include <CCommon.h>
-#include <CBluetooth.h>
 #include <CJoystick.h>
 #include <CMelodies.h>
 #include <CMotors.h>
-#include <CSDFilesManager.h>
-
-#define MAX72XX_TYPE MD_MAX72XX::FC16_HW
 
 // Test project just to test if libraries in this project compiles. Expendable class
 void setup()
 {
-    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
-    MD_MAX72XX max7219 = MD_MAX72XX(MAX72XX_TYPE, 11, 13, 10, 2);
-
     Common.ReadKeyboard();
-    Common.Sleep(500);
-    Common.PowerDownInt0();
+    MD_MAX72XX max7219 = MD_MAX72XX(MD_MAX72XX::FC16_HW, 11, 13, 10, 2);
 
-    int i = TrueRandom.random(1, 13);
     cppQueue q = cppQueue(sizeof(int), 10, FIFO);
     LinkedList<int> lnk = LinkedList<int>();
 
-    CBluetooth bluetooth = CBluetooth(i, 3, EBluetoothAdapter::HC_05);
-    CJoystick joystick = CJoystick(i, 3, 5);
-    CMelodies melodies = CMelodies(i);
+    CJoystick joystick = CJoystick(LED_BUILTIN, 3, 5);
+    CMelodies melodies = CMelodies(1);
     CMotors motors = CMotors();
-    CSDFilesManager files = CSDFilesManager(4);
 }
 
 void loop()
